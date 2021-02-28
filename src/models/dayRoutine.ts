e0ea64709fa8a596e18d3ca4406f62fe68975e9f
javier bustamante <javier.bustamante@intelygenz.com>
2021-02-28 16:40:36 +0100

    Add: Domain Models

---
+++ src/models/dayRoutine.ts
@@ -0,0 +1,38 @@
+enum DayOfTheWeek {
+  L = 'L',
+  M = 'M',
+  X = 'X',
+  J = 'J',
+  V = 'V',
+  S = 'S',
+  D = 'D',
+}
+export type DayRoutine = {
+  name: string
+  color: string
+  day: DayOfTheWeek
+  tasks: Task[]
+}
+
+export type Task = {
+  name: string
+  notes: string
+  hourStart: Date
+  hourEnd: Date
+  message: string
+}
+
+export type ActiveTask = {
+  referenced_task: Task
+  date: Date
+  mute: boolean
+}
+
+export type ActiveWeek = {
+  alerts_actived: boolean
+  notifs_actived: boolean
+  sound_actived: boolean
+  sound: string
+  ActiveTasks: { [key in keyof typeof DayOfTheWeek]: ActiveTask[] }
+  routines: DayRoutine[]
+}
